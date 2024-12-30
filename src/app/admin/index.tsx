12d7840bd2d19b113d81a9336b927396b03035bc
Med Mehdi Ben Haj Saleh <medmehdibenhajsaleh1@gmail.com>
2024-12-30 02:02:36 +0100

    Initial commit

--- src/app/admin/index.tsx
+++ src/app/admin/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { supabase } from "../../lib/supabase";
+import { supabase } from "@/lib/supabase";
 import Projects from "./Projects"; // Assume you have this component for uploading images
 import ContactTable from "./Contact"; // The table to show contacts
 import Image from "next/image";
